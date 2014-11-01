8d657d750829f64e41e50d68405012ff74349342
wasit7 <wasit7@gmail.com>
2014-11-01 13:29:36 +0700

    week11

---
+++ examples/week11_calib1.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 01 11:43:41 2014
+
+@author: Wasit
+"""
+
+import numpy as np
+import cv2
+import glob
+
+# termination criteria
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+nx=7#number of corner
+ny=6
+objp = np.zeros((nx*ny,3), np.float32)
+objp[:,:2] = np.mgrid[0:nx,0:ny].T.reshape(-1,2)
+
+# Arrays to store object points and image points from all the images.
+objpoints = [] # 3d point in real world space
+imgpoints = [] # 2d points in image plane.
+
+images = glob.glob('img/left*.jpg')
+
+for fname in images:
+    img = cv2.imread(fname)
+    print fname
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    # Find the chess board corners
+    
+    ret, corners = cv2.findChessboardCorners(gray, (nx,ny),None)
+
+    # If found, add object points, image points (after refining them)
+    if ret == True:
+        objpoints.append(objp)
+
+        cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
+        #imgpoints.append(corners2)
+
+        # Draw and display the corners
+        cv2.drawChessboardCorners(img, (nx,ny), corners,ret)
+        cv2.imshow('img',img)
+        cv2.waitKey(500)
+
+cv2.destroyAllWindows()
---
+++ examples/week11_tablet_chessboard.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 01 12:25:41 2014
+
+@author: Wasit
+"""
+
+import numpy as np
+import cv2
+
+# termination criteria
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+nx=5#number of corner
+ny=5
+objp = np.zeros((nx*ny,3), np.float32)
+objp[:,:2] = np.mgrid[0:nx,0:ny].T.reshape(-1,2)
+
+# Arrays to store object points and image points from all the images.
+objpoints = [] # 3d point in real world space
+imgpoints = [] # 2d points in image plane.
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+    # Capture frame-by-frame
+    ret, img = cap.read()
+
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+    # Find the chess board corners
+    find_flag= cv2.CALIB_CB_ADAPTIVE_THRESH + \
+    cv2.CALIB_CB_NORMALIZE_IMAGE + cv2.CALIB_CB_FAST_CHECK
+    ret, corners = cv2.findChessboardCorners(gray, (nx,ny),None)
+
+    # If found, add object points, image points (after refining them)
+    if ret == True:
+        objpoints.append(objp)
+
+        cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
+        #imgpoints.append(corners2)
+
+        # Draw and display the corners
+        cv2.drawChessboardCorners(img, (nx,ny), corners,ret)
+    cv2.imshow('img',img)    
+        
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
