67f84b6d02c43ec20fb6f032bd6d96b7776ccbbe
wasit7 <wasit7@gmail.com>
2014-12-28 12:21:52 +0700

    Add FFT

---
+++ examples/FFT/varying_image_size.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 24 17:48:17 2014
+
+@author: Wasit
+"""
+from PIL import Image
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy.ndimage import zoom
+ima=np.array(Image.open('../img/left01.jpg').convert('L'))
+imb=np.array(Image.open('../img/left01_h.jpg').convert('L'))
+
+#frequency domain
+fa=np.log(np.abs(np.fft.fftshift(np.fft.fft2(ima))))
+fb=np.log(np.abs(np.fft.fftshift(np.fft.fft2(imb))))
+#scaling
+sa=(200./fa.shape[0],200./fa.shape[1])
+sb=(200./fb.shape[0],200./fb.shape[1])
+#normalized frequency domian
+ga=zoom(fa,sa,order = 2)
+gb=zoom(fb,sb,order = 2)
+
+fig1=plt.figure()
+plt.imshow(ga,interpolation='none')
+plt.set_cmap('gray')
+
+fig2=plt.figure()
+plt.imshow(gb,interpolation='none')
+plt.set_cmap('gray')
+
+#f_FFT=2*width/wavelength
