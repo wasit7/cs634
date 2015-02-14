94ae7c1a36f6c7f7d1adf3b35c7e26ba415eb755
wasit7 <wasit7@gmail.com>
2015-02-14 11:56:26 +0700

    end of term

--- examples/FFT/varying_image_size.py
+++ examples/FFT/varying_image_size.py
@@ -8,15 +8,15 @@ from PIL import Image
 import numpy as np
 from matplotlib import pyplot as plt
 from scipy.ndimage import zoom
-ima=np.array(Image.open('../img/left01.jpg').convert('L'))
+ima=np.array(Image.open('../img/book.jpg').convert('L'))
 imb=np.array(Image.open('../img/left01_h.jpg').convert('L'))
 
 #frequency domain
 fa=np.log(np.abs(np.fft.fftshift(np.fft.fft2(ima))))
 fb=np.log(np.abs(np.fft.fftshift(np.fft.fft2(imb))))
 #scaling
-sa=(200./fa.shape[0],200./fa.shape[1])
-sb=(200./fb.shape[0],200./fb.shape[1])
+sa=(100./fa.shape[0],100./fa.shape[1])
+sb=(100./fb.shape[0],100./fb.shape[1])
 #normalized frequency domian
 ga=zoom(fa,sa,order = 2)
 gb=zoom(fb,sb,order = 2)
@@ -26,7 +26,7 @@ plt.imshow(ga,interpolation='none')
 plt.set_cmap('gray')
 
 fig2=plt.figure()
-plt.imshow(gb,interpolation='none')
+plt.imshow(fa,interpolation='none')
 plt.set_cmap('gray')
 
 #f_FFT=2*width/wavelength
